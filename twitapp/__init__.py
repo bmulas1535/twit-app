eb2cb2c56208751d4115618ae2521a6a514276a5
bmulas1535 <bmulas1535@users.noreply.github.com>
2019-11-14 13:22:25 -0800

    fix init database variable for the 5th time

--- twitapp/__init__.py
+++ twitapp/__init__.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from decouple import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -9,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1d06cefddc423f3464396352ac04df66'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
